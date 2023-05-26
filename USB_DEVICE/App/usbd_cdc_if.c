dc7a07b788fa43820a3136306fb0dd082a70f569
Elia Palme <2025999+theely@users.noreply.github.com>
2023-05-26 15:34:12 +0200

    added usb serial configuration

--- USB_DEVICE/App/usbd_cdc_if.c
+++ USB_DEVICE/App/usbd_cdc_if.c
@@ -34,6 +34,16 @@
 #define CMD_BUFFER_SIZE 64 // size of the input buffer
 char cmd_buffer[CMD_BUFFER_SIZE];
 int8_t cmd_buffer_index = 0;
+
+// 115200bps, 1stop, no parity, 8bit
+USBD_CDC_LineCodingTypeDef LineCoding = {
+  115200,                       /* baud rate: 115200 */
+  0x00,                         /* stop bits: 1 */
+  0x00,                         /* parity: none */
+  0x08                          /* nb. of bits: 8 */
+};
+
+
 /* USER CODE END PV */
 
 /** @addtogroup STM32_USB_OTG_DEVICE_LIBRARY
@@ -222,11 +232,22 @@ static int8_t CDC_Control_FS(uint8_t cmd, uint8_t* pbuf, uint16_t length)
   /* 6      | bDataBits  |   1   | Number Data bits (5, 6, 7, 8 or 16).          */
   /*******************************************************************************/
     case CDC_SET_LINE_CODING:
-
+        LineCoding.bitrate = (uint32_t) (pbuf[0] | (pbuf[1] << 8) |
+                                         (pbuf[2] << 16) | (pbuf[3] << 24));
+        LineCoding.format = pbuf[4];
+        LineCoding.paritytype = pbuf[5];
+        LineCoding.datatype = pbuf[6];
+        break;
     break;
 
     case CDC_GET_LINE_CODING:
-
+        pbuf[0] = (uint8_t) (LineCoding.bitrate);
+        pbuf[1] = (uint8_t) (LineCoding.bitrate >> 8);
+        pbuf[2] = (uint8_t) (LineCoding.bitrate >> 16);
+        pbuf[3] = (uint8_t) (LineCoding.bitrate >> 24);
+        pbuf[4] = LineCoding.format;
+        pbuf[5] = LineCoding.paritytype;
+        pbuf[6] = LineCoding.datatype;
     break;
 
     case CDC_SET_CONTROL_LINE_STATE:
